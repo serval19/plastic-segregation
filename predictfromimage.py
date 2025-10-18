1c3627484a5f424264c2a9862ff0007763218dc3
serval19 <diwatomy04@gmail.com>
2025-10-18 19:55:06 +0530

    added code for predicting the class from an image

---
+++ predictfromimage.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+import os
+
+# Then your detection code
+model = YOLO("./trainresults/weights/best.pt")
+test_image_path = "./testimage/16.jpg"
+results = model.predict(source=test_image_path, save=True)
